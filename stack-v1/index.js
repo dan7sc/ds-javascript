6ac80e0844e206de440334d9a47a53f476498e3f
dan7sc <su7cheng@gmail.com>
2020-07-08 20:15:25 -0300

    First commit - Add stack class using array

---
+++ stack-v1/index.js
@@ -0,0 +1,26 @@
+const Stack = require('./stack')
+
+const stack = new Stack()
+console.log(stack.isEmpty())
+
+stack.push(4)
+stack.push(5)
+stack.push(1)
+
+console.log(stack)
+console.log(stack.peek())
+
+stack.push(3)
+stack.push(9)
+
+console.log(stack)
+console.log(stack.peek())
+console.log(stack.size())
+console.log(stack.isEmpty())
+
+stack.pop()
+stack.pop()
+
+console.log(stack)
+console.log(stack.peek())
+console.log(stack.size())
