544cc79ddc6ce07881e36805856a5c84dace9e03
dan7sc <su7cheng@gmail.com>
2020-10-19 07:52:44 -0300

    Add insertion sort

---
+++ sorting/insertion-sort-v1/insertion-sort.js
@@ -0,0 +1,27 @@
+const {
+  defaultCompare,
+  Compare,
+  createNonSortedArray
+} = require('../../util')
+
+const insertionSort = (array, compareFn = defaultCompare) => {
+  const { length } = array
+  let temp
+
+  for(let i = 1; i < length; i++) {
+    let j = i
+    temp = array[i]
+    while(j > 0 && compareFn(array[j - 1], temp) === Compare.BIGGER_THAN) {
+      array[j] = array[j - 1]
+      j--
+    }
+    array[j] = temp
+  }
+
+  return array
+}
+
+let array = createNonSortedArray(10)
+console.log(array.join(', '))
+array = insertionSort(array)
+console.log(array.join(', '))
